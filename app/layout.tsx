45a15e1fb54e74c6d5d8e7a888ef536be472df3d
husongtao <15309487104@163.com>
2024-11-01 22:49:50 +0800

    refactor: 文件归类

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NextUIProvider from "./NextUIProvider";
+import NextUIProvider from "../providers/NextUIProvider";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { CounterStoreProvider } from "@/providers/counter-store-provider";
 
