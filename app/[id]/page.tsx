9246a600f06838cf6a7cbf2b46bca34873e9f4e2
husongtao <15309487104@163.com>
2024-11-03 22:34:03 +0800

    feat: 商品详情页面

---
+++ app/[id]/page.tsx
@@ -0,0 +1,13 @@
+import ShopHeader from "@/components/ShopHeader";
+
+export default function CommodityDetail({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return (
+    <>
+      <ShopHeader />
+    </>
+  );
+}
